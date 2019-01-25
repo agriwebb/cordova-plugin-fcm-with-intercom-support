b63a6380bc107124dc5e8297dbd6de9bfff331a2
Eric Martinez de Morentin <eric.morentin@agriwebb.com>
2019-01-25 14:53:21 +1100

    Delegate to intercom whenever an intercom message is received.

--- src/android/MyFirebaseMessagingService.java
+++ src/android/MyFirebaseMessagingService.java
@@ -14,12 +14,16 @@ import java.util.HashMap;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import io.intercom.android.sdk.push.IntercomPushClient;
+
 /**
  * Created by Felipe Echanique on 08/06/2016.
  */
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "FCMPlugin";
+
+    private final IntercomPushClient intercomPushClient = new IntercomPushClient();
 
     /**
      * Called when message is received.
@@ -34,28 +38,35 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
         Log.d(TAG, "==> MyFirebaseMessagingService onMessageReceived");
-        
-        if(remoteMessage.getNotification() != null){
-            Log.d(TAG, "\tNotification Title: " + remoteMessage.getNotification().getTitle());
-            Log.d(TAG, "\tNotification Message: " + remoteMessage.getNotification().getBody());
-        }
-        
-        Map<String, Object> data = new HashMap<String, Object>();
-        data.put("wasTapped", false);
-        
-        if(remoteMessage.getNotification() != null){
-            data.put("title", remoteMessage.getNotification().getTitle());
-            data.put("body", remoteMessage.getNotification().getBody());
-        }
 
-        for (String key : remoteMessage.getData().keySet()) {
+        Map message = remoteMessage.getData();
+        if (intercomPushClient.isIntercomPush(message)) {
+            intercomPushClient.handlePush(getApplication(), message);
+        } else {
+            if(remoteMessage.getNotification() != null){
+                Log.d(TAG, "\tNotification Title: " + remoteMessage.getNotification().getTitle());
+                Log.d(TAG, "\tNotification Message: " + remoteMessage.getNotification().getBody());
+            }
+
+            Map<String, Object> data = new HashMap<String, Object>();
+            data.put("wasTapped", false);
+
+            if(remoteMessage.getNotification() != null){
+                data.put("title", remoteMessage.getNotification().getTitle());
+                data.put("body", remoteMessage.getNotification().getBody());
+            }
+
+            for (String key : remoteMessage.getData().keySet()) {
                 Object value = remoteMessage.getData().get(key);
                 Log.d(TAG, "\tKey: " + key + " Value: " + value);
                 data.put(key, value);
+            }
+
+            Log.d(TAG, "\tNotification Data: " + data.toString());
+            FCMPlugin.sendPushPayload( data );
+
         }
-        
-        Log.d(TAG, "\tNotification Data: " + data.toString());
-        FCMPlugin.sendPushPayload( data );
+
         //sendNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody(), remoteMessage.getData());
     }
     // [END receive_message]
